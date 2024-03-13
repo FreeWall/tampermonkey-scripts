5dcab76f6b5d8f20406d110371784f65814848cd
Michal Vaněk <info@michalvanek.net>
2024-03-13 13:49:43 +0100

    init

--- src/google-search-map-link/index.js
+++ src/google-search-map-link/index.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      2024-03-08
 // @description  Add missing link to map thumbnail element in Google Search. F*ck EU!
-// @match        *https://www.google.com/search?q*
+// @match        *https://www.google.com/search?*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=google.com
 // @require      https://raw.githubusercontent.com/FreeWall/tampermonkey-scripts/master/src/google-search-map-link/script.js
 // ==/UserScript==
