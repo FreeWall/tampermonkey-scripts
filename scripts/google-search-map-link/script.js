d5e3955a11bf8fc88822d7781e3bfacfcff33245
Michal Vanek <info@michalvanek.net>
2024-03-27 08:13:43 +0100

    google search map link - address type

--- scripts/google-search-map-link/script.js
+++ scripts/google-search-map-link/script.js
@@ -1,5 +1,6 @@
 window.__tampermonkeyscript_run = () => {
-    function updateMapLink() {
+    // business type
+    function resultsType1() {
         const addressSection = document.querySelector('div[data-attrid="kc:/location/location:address"]');
         if (!addressSection) {
             return;
@@ -16,6 +17,42 @@ window.__tampermonkeyscript_run = () => {
         if (luMapParentLink) {
             luMapParentLink.setAttribute('href', mapUrl);
         }
+    }
+
+    // address type
+    function resultsType2() {
+        const img = document.querySelector('div#lu_map');
+        if (!img) {
+            return;
+        }
+
+        const parent = img.closest('.dirs')?.parentElement;
+        if (!parent) {
+            return;
+        }
+
+        const mapLink = parent.querySelector('a[data-url]');
+        if (!mapLink) {
+            return;
+        }
+
+        const url = mapLink.getAttribute('data-url');
+        const addressPart = url.match(/\/maps\/dir\/\/([^\/?]+)/);
+        if (!addressPart) {
+            return;
+        }
+
+        const mapUrl = location.origin + '/maps/place/' + addressPart[1];
+        const link = document.createElement('a');
+        link.setAttribute('href', mapUrl);
+
+        img.parentNode.insertBefore(link, img);
+        link.appendChild(img);
+    }
+
+    function updateMapLink() {
+        resultsType1();
+        resultsType2();
     }
 
     if (document.readyState === 'loading') {
