12063e8a47d0a662a788fe51f2511cb28b2d4590
FreeWall <info@michalvanek.net>
2025-04-20 12:25:52 +0200

    google gemini

---
+++ scripts/google-gemini/index.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Google Gemini
+// @namespace    http://tampermonkey.net/
+// @version      2025-04-19
+// @description  Google Gemini app enhancements
+// @match        *https://gemini.google.com/app*
+// @icon         https://www.gstatic.com/lamda/images/gemini_favicon_f069958c85030456e93de685481c559f160ea06b.png
+// @require      https://raw.githubusercontent.com/FreeWall/tampermonkey-scripts/master/scripts/google-gemini/script.js
+// ==/UserScript==
+
+window.__tampermonkeyscript_run();
---
+++ scripts/google-gemini/script.js
@@ -0,0 +1,76 @@
+window.__tampermonkeyscript_run = () => {
+    let historyExpanded = false;
+    /**
+     * Expand the history section on first load
+     */
+    function expandHistory() {
+        if (historyExpanded) {
+            return;
+        }
+
+        const button = document.querySelector('[data-test-id="show-more-button"]');
+        if (!button) {
+            return;
+        }
+
+        historyExpanded = true;
+
+        button.click();
+        button.remove();
+    }
+
+    /**
+     * Do not collapse the side nav
+     */
+    let sideNavObserved = false;
+    function observeSideNav() {
+        if (sideNavObserved) {
+            return;
+        }
+
+        const sidenav = document.querySelector('.sidenav-with-history-container');
+        if (!sidenav) {
+            return;
+        }
+
+        sideNavObserved = true;
+
+        const observer = new MutationObserver((mutationsList, observer) => {
+            let isCollapsed = false;
+            for (const mutation of mutationsList) {
+                if (mutation.type === 'attributes' && mutation.attributeName === 'class') {
+                    if (sidenav.className.includes('collapsed')) {
+                        isCollapsed = true;
+                    }
+                }
+            }
+
+            if (isCollapsed) {
+                const sideNavButton = document.querySelector('[data-test-id="side-nav-menu-button"]');
+                if (!sideNavButton) {
+                    return;
+                }
+
+                sideNavButton.click();
+            }
+        });
+
+        observer.observe(sidenav, { attributes: true, attributeOldValue: true });
+    }
+
+    const appRoot = document.getElementById('app-root');
+    if (!appRoot) {
+        return;
+    }
+
+    const observer = new MutationObserver((mutationsList, observer) => {
+        for (const mutation of mutationsList) {
+            if (mutation.type === 'childList') {
+                expandHistory();
+                observeSideNav();
+            }
+        }
+    });
+
+    observer.observe(appRoot, { childList: true, subtree: true });
+};
